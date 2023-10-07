ce8e417a8888609e4937641962b75a94e6325e85
Andrei-David Nan <nandavid190@gmail.com>
2023-10-07 13:39:23 +0300

    v0.3.4 - Added tests for WriteJSON;

--- helpers/helpers_test.go
+++ helpers/helpers_test.go
@@ -79,6 +79,46 @@ func TestReadJSON(t *testing.T) {
 			t.Error("ReadJSON() expected an error, got nil")
 		}
 	})
+}
 
-	s
+func TestWriteJSON(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Valid JSON", func(t *testing.T) {
+		data := struct {
+			Name string `json:"name"`
+		}{Name: "John"}
+
+		w := httptest.NewRecorder()
+
+		err := WriteJSON(w, http.StatusOK, data)
+
+		if err != nil {
+			t.Errorf("WriteJSON() error = %v, want nil", err)
+		}
+
+		if w.Code != http.StatusOK {
+			t.Errorf("WriteJSON() status code = %d, want %d", w.Code, http.StatusOK)
+		}
+
+		expectedJSON := `{
+			"name": "John"
+		}`
+
+		if w.Body.String() != expectedJSON {
+			t.Errorf("WriteJSON() response body = %s, want %s", w.Body.String(), expectedJSON)
+		}
+	})
+
+	t.Run("Invalid JSON Encoding", func(t *testing.T) {
+		invalidData := make(chan int) // Unsupported type for JSON encoding
+
+		w := httptest.NewRecorder()
+
+		err := WriteJSON(w, http.StatusOK, invalidData)
+
+		if err == nil {
+			t.Error("WriteJSON() expected an error, got nil")
+		}
+	})
 }
