71765724e317e12340a4d8030238ba5ce65341f4
Andrei-David Nan <nandavid190@gmail.com>
2023-10-22 10:56:30 +0300

    v0.5.0 - Added GET and POST endpoints;

--- controllers/coffees.go
+++ controllers/coffees.go
@@ -1,1 +1,40 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/davidandw190/coffeeshop-api-go/helpers"
+	"github.com/davidandw190/coffeeshop-api-go/services"
+)
+
+var coffee services.Coffee
+
+// GET/coffees
+func GetAllCoffees(w http.ResponseWriter, r *http.Request) {
+	coffees, err := coffee.GetAllCoffees()
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+	}
+
+	helpers.WriteJSON(w, http.StatusOK, helpers.Envelope{"coffees": coffees})
+}
+
+// POST/coffees/coffee
+func CreateCoffee(w http.ResponseWriter, r *http.Request) {
+	var coffeeData services.Coffee
+	err := json.NewDecoder(r.Body).Decode(&coffeeData)
+
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+		return
+	}
+
+	coffeeCreated, err := coffee.CreateCoffee(coffeeData)
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+		return
+	}
+
+	helpers.WriteJSON(w, http.StatusOK, coffeeCreated)
+}
